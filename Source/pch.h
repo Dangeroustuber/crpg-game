7925ad6d8f7c082ef3b69ad4003d22852679ba9e
Elvis Arifagic <slewision@gmail.com>
2024-11-20 19:55:11 +0100

    Add precompiled header

---
+++ Source/pch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <filesystem>
+#include <cstdint>
+#include <memory>
+#include <span>
+
+#include <directx/d3d12.h>
+#include <directx/d3dx12.h>
+#include <wrl/client.h>
+
+#include "Utils/Utils.hpp"
+#include "Engine/Subsystems/Logger.hpp"
